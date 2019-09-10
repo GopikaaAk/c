803feaeebc88f2c4f31a826baebf2624f901bb9b
GopikaaAk <55086674+GopikaaAk@users.noreply.github.com>
2019-09-10 14:36:48 +0530

    Update swap alternate elmt.c

--- swap alternate elmt.c
+++ swap alternate elmt.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+int main()
+{
+int total;
+int i;
+int temp;
+scanf("%d",&total);
+int array[total];
+for(i=0;i<total;i++)
+{
+scanf("%d",&array[i]);
+}
+for(i=0;i<total;i+=2)
+{
+if(i+1==total)
+{
+break;
+}
+temp=array[i];
+array[i]=array[i+1];
+array[i+1]=temp;
+}
+for(i=0;i<total;i++)
+{
+printf("%d",array[i]);
+}
+return 0;
+}
